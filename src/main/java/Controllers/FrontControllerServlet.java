2e4b86027518682d4e1abf484f1f217dd3c7ff43
arontsoaMendrika <mendrikaarontsoa@gmail.com>
2026-07-12 21:57:12 +0300

    Sprint 5 : ajout de ModelAndView

--- src/main/java/Controllers/FrontControllerServlet.java
+++ src/main/java/Controllers/FrontControllerServlet.java
@@ -3,64 +3,89 @@ package main.java.Controllers;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.lang.reflect.Method;
+import main.java.util.ModelAndView; 
 
 public class FrontControllerServlet extends HttpServlet {
 
     private HashMap<String, Method> mappingUrls;
+    private String prefix;
+    private String suffix;
 
     @Override
     @SuppressWarnings("unchecked")
     public void init() throws ServletException {
         super.init();
         this.mappingUrls = (HashMap<String, Method>) getServletContext().getAttribute("tableRoutage");
+        
+        this.prefix = getServletConfig().getInitParameter("prefix");
+        this.suffix = getServletConfig().getInitParameter("suffix");
+        
         if (this.mappingUrls == null) {
             throw new ServletException("La table de routage n'a pas pu être récupérée du ServletContextListener !");
         }
-        System.out.println("Servlet prête et connectée à la table de routage.");
+        System.out.println("Servlet prête et connectée à la table de routage. Configuration : prefix = " + this.prefix + ", suffix = " + this.suffix);
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            String uri = request.getRequestURI();
-            String contextPath = request.getContextPath();
-            String path = uri.substring(contextPath.length());
-            String requeteMethod = request.getMethod(); // GET ou POST
+        
+        String uri = request.getRequestURI();
+        String contextPath = request.getContextPath();
+        String path = uri.substring(contextPath.length());
+        String requeteMethod = request.getMethod(); // GET ou POST
 
-            String cleRecherchee = requeteMethod + ":" + path;
+        String cleRecherchee = requeteMethod + ":" + path;
 
-            if (mappingUrls.containsKey(cleRecherchee)) {
-                Method method = mappingUrls.get(cleRecherchee);
-                Class<?> clazz = method.getDeclaringClass();
+        if (mappingUrls.containsKey(cleRecherchee)) {
+            Method method = mappingUrls.get(cleRecherchee);
+            Class<?> clazz = method.getDeclaringClass();
 
-                try {
-                    Object controleurInstance = clazz.getDeclaredConstructor().newInstance();
-                    method.invoke(controleurInstance);
-                    out.println("<html><body>");
-                    out.println("<h1>URI: " + uri + "</h1>");
-                    out.println("<h2>[Succès] Méthode de Contrôleur exécutée :</h2>");
-                    out.println("<table border='1' cellpadding='8' style='border-collapse: collapse;'>");
-                    out.println("<tr style='background-color: #e6f7ff;'><th>URL annoté</th><th>Classe contrôleur</th><th>Méthode associée</th></tr>");
-                    out.println("<tr>");
-                    out.println("<td><strong>" + path + "</strong></td>");
-                    out.println("<td>" + clazz.getName() + "</td>");
-                    out.println("<td>" + method.getName() + "()</td>");
-                    out.println("</tr>");
-                    out.println("</table>");
-                    out.println("</body></html>");
-                    
-                } catch (Exception e) {
-                    throw new ServletException("Erreur lors de l'exécution de la méthode " + method.getName() + "()", e);
+            try {
+                Object controleurInstance = clazz.getDeclaredConstructor().newInstance();
+                
+                Object resultatInvocation = method.invoke(controleurInstance);
+
+                if (resultatInvocation instanceof ModelAndView) {
+                    ModelAndView mv = (ModelAndView) resultatInvocation;
+                    if (mv.getModel() != null) {
+                        for (Map.Entry<String, Object> entry : mv.getModel().entrySet()) {
+                            request.setAttribute(entry.getKey(), entry.getValue());
+                        }
+                    }
+                    String cheminJsp = this.prefix + mv.getViewName() + this.suffix;
+
+                  RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher(cheminJsp);
+
+                    if (dispatcher != null) {
+                        dispatcher.forward(request, response);
+                        return; 
+                    } else {
+                        throw new ServletException("Impossible de trouver le dispatcher pour le chemin : " + cheminJsp);
+                    }
+                } else {
+                    try (PrintWriter out = response.getWriter()) {
+                        out.println("<html><body>");
+                        out.println("<h1>Erreur d'architecture</h1>");
+                        out.println("<p>La méthode " + method.getName() + "() n'a pas retourné un objet ModelAndView.</p>");
+                        out.println("</body></html>");
+                    }
                 }
-            } else {
+
+            } catch (Exception e) {
+                throw new ServletException("Erreur lors de l'exécution de la méthode " + method.getName() + "()", e);
+            }
+        } else {
+            try (PrintWriter out = response.getWriter()) {
                 out.println("<html><body>");
                 out.println("<h1>URI: " + uri + "</h1>");
                 out.println("<h2>Aucun mapping trouvé pour la combinaison : <span style='color:red;'>" + cleRecherchee + "</span></h2>");
